001c8359d28c3def28c7d57402a91b65868e4cda
rust42 <sndpkrl007@hotmail.com>
2024-01-18 21:13:49 -0600

    Json from s3

--- BlogsFunction/src/main/java/blogs/App.java
+++ BlogsFunction/src/main/java/blogs/App.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -12,6 +14,11 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 
 public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
@@ -22,17 +29,25 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
         headers.put("Access-Control-Allow-Origin", "*");
         headers.put("Access-Control-Allow-Credentials", "true,");
 
+        S3Client client = S3Client.builder()
+                .region(Region.US_EAST_1)
+                .build();
+        GetObjectRequest request = GetObjectRequest.builder()
+                .key("post_1.json")
+                .bucket("blogs-posts-json")
+                .build();
+
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
         try {
-            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
+            ResponseBytes<GetObjectResponse> objectBytes = client.getObjectAsBytes(request);
+            String responseJson = objectBytes.asString(StandardCharsets.UTF_8);
 
 
             return response
                     .withStatusCode(200)
-                    .withBody(output);
-        } catch (IOException e) {
+                    .withBody(responseJson);
+        } catch (Exception e) {
             return response
                     .withBody("{}")
                     .withStatusCode(500);
