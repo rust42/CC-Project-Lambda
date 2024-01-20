b4f835360a074ddfbcbea8ba84a8f84a7087fb3e
rust42 <sndpkrl007@hotmail.com>
2024-01-20 03:08:40 -0600

    Contact mapping

---
+++ BlogsFunction/src/main/java/blogs/Contact.java
@@ -0,0 +1,63 @@
+package blogs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+
+public class Contact implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+        headers.put("Access-Control-Allow-Origin", "*");
+        headers.put("Access-Control-Allow-Credentials", "true,");
+
+        S3Client client = S3Client.builder()
+                .region(Region.US_EAST_1)
+                .build();
+        GetObjectRequest request = GetObjectRequest.builder()
+                .key("post_1.json")
+                .bucket("blogs-posts-json")
+                .build();
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+        try {
+            ResponseBytes<GetObjectResponse> objectBytes = client.getObjectAsBytes(request);
+            String responseJson = objectBytes.asString(StandardCharsets.UTF_8);
+
+
+            return response
+                    .withStatusCode(200)
+                    .withBody(responseJson);
+        } catch (Exception e) {
+            return response
+                    .withBody("{}")
+                    .withStatusCode(500);
+        }
+    }
+
+    private String getPageContents(String address) throws IOException{
+        URL url = new URL(address);
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            return br.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+    }
+}
